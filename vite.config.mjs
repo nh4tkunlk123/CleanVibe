220bdd1acae7dc247173f8095f3edc61cba83735
nh4tkunlk123 <nh4tkunlk789@gmail.com>
2025-11-19 00:38:00 +0700

    2

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,10 @@
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 const repoName = 'CleanVibe'; 
 
 export default defineConfig({
   plugins: [react()],
- 
   base: `/${repoName}/`, 
-})
+});
